85fbaab5ccbdefefc39679a8d3dd7ecf577eaef3
Kamal Ahmed <Ahmed_Syed@bah.com>
2019-09-25 00:23:57 -0400

    working with python 3.6.8 and pip3

--- Vagrantfile
+++ Vagrantfile
@@ -8,13 +8,15 @@ require 'vagrant-hostmanager'
 Vagrant.configure(2) do |config|
 
   # Set base box and provisioning
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "ubuntu/bionic64"
   config.vm.provision :shell, path: "bootstrap.sh"
   #config.vm.network "forwarded_port", guest: 80, host: 80
+  config.vm.network :private_network, ip: '192.168.10.12'
+  #config.vm.network :private_network, ip: "192.168.68.8"
  
   # Provider resources
   config.vm.provider "virtualbox" do |v|
-    v.name = "devbox.spacewalk.dev"
+    v.name = "devbox.bah.com"
     v.memory = 1024
     v.cpus = 2
   end
@@ -22,11 +24,13 @@ Vagrant.configure(2) do |config|
   # Networking
   config.hostmanager.enabled = true
   config.hostmanager.manage_host = true
-  config.vm.hostname = 'devbox.spacewalk.dev'
-  config.vm.network :private_network, ip: '192.168.10.12'
-  #config.vm.network "forwarded_port", guest: 80, host: 80
+  config.vm.hostname = 'devbox.bah.com'
+  config.vm.provision :shell, :inline => "apt-get update --fix-missing"
+  config.vm.provision :shell, :inline => "sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y python3 python3-pip"
+  config.vm.provision :shell, :inline => "sudo DEBIAN_FRONTEND=noninteractive echo 'alias python=/usr/bin/python3' >> /home/vagrant/.bashrc"
+  config.vm.provision :shell, :inline => "sudo DEBIAN_FRONTEND=noninteractive echo 'alias pip=/usr/bin/pip3' >> /home/vagrant/.bashrc"
+  #config.vm.network :private_network, ip: '192.168.10.12'
   config.vm.network :forwarded_port, guest: 80, host: 8080
   # Shared folder
-  #config.vm.synced_folder '.', '/Users/a3x52zz/DEVOPS/TOOLS/VAGRANT/UBUNTU/vagrant-apache/vagrant'
- 
+  config.vm.synced_folder '.', '/Users/syedahmed/DEVOPS/TOOLS/VAGRANT/UBUNTU/vagrant-apache/vagrant'
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
 sed -i '/^#.*force_color_prompt=yes/s/^#//' ./.bashrc
-#echo "ServerName localhost" > /etc/apache2/httpd.conf
+echo "ServerName localhost" > /etc/apache2/httpd.conf
 mkdir -p /var/www/html
 echo "cd /var/www/html/" >> ./.bashrc
 
@@ -15,10 +15,34 @@ printf "%$(tput cols)s\n"|tr " " "="
 echo "Installing Apache and its modules"
 printf "%$(tput cols)s\n"|tr " " "="
 apt-get install -y apache2
-#apt-get install -y php5
+apt-get install -y php5
 #apt-get install -y php5-xdebug
 
-echo "ServerName localhost" > /etc/apache2/httpd.conf
+#printf "installing python 3.7.1 ...."
+#apt-get update
+#apt install software-properties-common
+#apt-get install python3.7
+#echo "alias python=/usr/bin/python3" >> /home/vagrant/.bashrc
+#source ~/home/vagrant/.bashrc
+#apt-get install python-pip
+
+#apt-get install -y wget 
+#sudo apt install zlib1g-dev
+#wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
+#tar -xvf Python-3.7.3.tgz
+#sudo apt-get install gcc
+#cd Python-3.7.3
+#./configure
+#make
+#sudo make install
+#sudo apt-get upgrade python3
+#echo "alias python=/usr/bin/python3" >> /home/vagrant/.bashrc
+#source ~/home/vagrant/.bashrc
+
+
+# installing python dev environment
+#sudo apt-get install build-essential libssl-dev libffi-dev python-dev -y
+#sudo apt install python3-pip
 
 # Setup hosts file
 VHOST=$(cat <<EOF
@@ -41,9 +65,9 @@ service apache2 restart
 printf "%$(tput cols)s\n"|tr " " "="
 echo "Installing MySql"
 printf "%$(tput cols)s\n"|tr " " "="
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password vagrant'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password vagrant'
-sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password vagrant'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password vagrant'
+apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
 
 
 printf "%$(tput cols)s\n"|tr " " "="
@@ -56,9 +80,9 @@ echo "Configuring Apache for Vagrant"
 #sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 a2enmod rewrite
 #Configuring XDebug
-#echo "zend_extension=xdebug.so">/etc/php5/mods-available/xdebug.ini
-#echo "xdebug.remote_enable = on">>/etc/php5/mods-available/xdebug.ini
-#echo "xdebug.remote_connect_back = on">>/etc/php5/mods-available/xdebug.ini
+echo "zend_extension=xdebug.so">/etc/php5/mods-available/xdebug.ini
+echo "xdebug.remote_enable = on">>/etc/php5/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back = on">>/etc/php5/mods-available/xdebug.ini
 
 service apache2 restart
 
